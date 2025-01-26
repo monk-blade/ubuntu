3534bb4b328cf194ff602f43e38fede8a6b0aeb1
monk-blade <arpanchavdaeng@gmail.com>
2025-01-26 19:42:22 +0530

    add yazi and starship

--- home/.chezmoiscripts/run_once_after_ubuntu_40_install_mise.sh
+++ home/.chezmoiscripts/run_once_after_ubuntu_40_install_mise.sh
@@ -11,3 +11,14 @@ echo ""
 wget -qO- https://mise.run/ | sh
 
 mise install --yes 
+
+echo ""
+echo "==============================================================="
+echo ""
+echo "Installing starship prompt"
+echo ""
+echo "==============================================================="
+echo ""
+
+
+curl -sS https://starship.rs/install.sh | sh
