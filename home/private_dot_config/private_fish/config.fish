15bda2e720f5de97283672ffe25bdc1877f4a316
monk-blade <arpanchavdaeng@gmail.com>
2025-01-26 19:51:16 +0530

    fix mise and typo

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -8,13 +8,14 @@ source $HOME/.config/fish/abbr.fish
 zoxide init fish | source
 
 #mise setup 
-/usr/bin/mise activate fish | source
+$HOME/.local/bin/mise activate fish | source
 
 #Rust setup
-source "$HOME/.cargo/env.fish"
+# source "$HOME/.cargo/env.fish"
 
 # Setup starship prompt
 starship init fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
